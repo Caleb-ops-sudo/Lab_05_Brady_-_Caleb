7d104d0b8c7523f429bd20991909b069796ae3b9
Caleb-ops-sudo <calebcook766@u.boisestate.edu>
2025-02-11 11:22:46 -0700

    Create light.v

---
+++ light.v
@@ -0,0 +1,8 @@
+module light(
+    input A, B,   // Declare downstairs and upstairs input
+    output Y    // Declare stair light output
+);
+    assign Y = A^B;
+    // Enter logic equation here
+
+endmodule
